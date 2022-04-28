e19fbd701e8cdb622b976c79d14e8abb18432410
vkg6614 <vkg6614@gmail.com>
2022-04-28 17:35:59 +0530

    cart page commit

--- src/components/CartDetails/CartDetails.jsx
+++ src/components/CartDetails/CartDetails.jsx
@@ -1,7 +1,53 @@
+import { Delete } from "@mui/icons-material";
 import React from "react";
+import ResponsiveAppBar from "../CartNavbar/CartNavbar";
+import "./CartDetails.css";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const CartDetails = () => {
-  return <div>CartDetails</div>;
+  return (
+    <div>
+      <ResponsiveAppBar />
+      <div className="cartDetails-main-container">
+        <div className="cartDetails-secondary-container">
+          <div className="cartDetails-left-container">
+            <h3>My Shopping Bag(3)</h3>
+            <div>
+              <div className="single-pro">
+                <div style={{ border: "1px solid black", width: "130px" }}>
+                  <img width="130px" src="" alt="" />
+                </div>
+                <div>
+                  <p>Blue Bell Sleeve Jacquard Wrap Top</p>
+                  <div className="small-details">
+                    <p>Size</p>
+                    <p>Quantity</p>
+                    <p>Color</p>
+                  </div>
+                  <div style={{ borderBottom: "2px solid grey" }}>
+                    <p>SKU CODE</p>
+                  </div>
+
+                  <div className="add-del-div">
+                    <p>Edit Item</p>
+                    <p>Move To Wishlist</p>
+                  </div>
+                </div>
+                <div>
+                  <p className="price">Price 1200</p>
+                  <DeleteIcon />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="cartDetails-right-container">
+            <h3>Price Details</h3>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartDetails;
---
+++ src/components/CartNavbar/CartNavbar.jsx
@@ -0,0 +1,111 @@
+import * as React from "react";
+import "./CartNavbar.css";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import PaymentIcon from "@mui/icons-material/Payment";
+
+// const pages = ["Products", "Pricing", "Blog"];
+const pages = [
+  { id: 1, icon: <AddShoppingCartIcon />, text: "Bag" },
+  { id: 2, icon: <LocalShippingIcon />, text: "Shipping" },
+  { id: 3, icon: <PaymentIcon />, text: "Payment" },
+];
+
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar style={{ padding: "0", border: "1px solid black" }}>
+      <Container
+        maxWidth="xl"
+        style={{
+          backgroundColor: "grey",
+          padding: "0px",
+        }}
+      >
+        <Toolbar
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            padding: "0",
+          }}
+        >
+          <img
+            style={{ width: "10%" }}
+            src="https://www.faballey.com/images/logo.png"
+            alt="faballey-img1"
+          />
+
+          <Box className="details-div">
+            {pages.map((page) => (
+              <Button key={page.id} sx={{ color: "white", display: "block" }}>
+                {page.icon}
+                <p>{page.text}</p>
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default ResponsiveAppBar;
