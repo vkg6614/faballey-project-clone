d875cae8664e8a11e049cbada0e697a81c024d98
vkg6614 <vkg6614@gmail.com>
2022-04-28 20:22:53 +0530

    commit agian

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -38,7 +38,7 @@ const ProductDetails = () => {
       hoverImages,
     } = proData[0];
 
-    axios.post("http://localhost:4000/cartsData", {
+    axios.post("https://faballey-jsonserver-reactjs.herokuapp.com/cartsData", {
       title,
       color: prodSize,
       categoryId,
